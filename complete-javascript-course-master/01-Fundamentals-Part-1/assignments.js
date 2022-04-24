4a45baea17a6a435b30d52e85c2ff957d8946e2a
allisondev16 <allisonmamangun@gmail.com>
2022-04-24 22:11:44 +0800

    Update assignment.js

--- complete-javascript-course-master/01-Fundamentals-Part-1/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/assignments.js
@@ -139,3 +139,18 @@ if (population > 33) {
 let value = 1
 console.log('value', value++);
 
+// LECTURE: Type Conversion and Coercion
+// 1. Predict the result of these 5 operations without executing them:
+// '9' - '5';
+// '19' - '13' + '17';
+// '19' - '13' + 17;
+// '123' < 57;
+// 5 + 6 + '4' + 9 - 4 - 2;
+// 2. Execute the operations to check if you were right
+
+
+'9' - '5'; // 4
+'19' - '13' + '17'; // '617'
+'19' - '13' + 17; // 23
+'123' < 57; // false
+5 + 6 + '4' + 9 - 4 - 2; // 1143
