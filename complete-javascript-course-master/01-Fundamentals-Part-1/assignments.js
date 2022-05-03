01124a66f4f4ed513928ba04a407331e008734b0
allisondev16 <allisonmamangun@gmail.com>
2022-05-03 19:57:10 +0800

    Update assignments.js

--- complete-javascript-course-master/01-Fundamentals-Part-1/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/assignments.js
@@ -6,7 +6,7 @@
 
 const country = "Philippines"
 const continent = "Asia"
-let population = 13
+let population = 130
 
 console.log(country)
 console.log(continent)
@@ -267,3 +267,68 @@ if (day === 'monday') {
 } else {
     console.log("Not a valid day");
 }
+
+
+// LECTURE: The switch Statement
+// 1. Use a switch statement to log the following string for the given 'language':
+// chinese or mandarin: 'MOST number of native speakers!'
+// spanish: '2nd place in number of native speakers'
+// english: '3rd place'
+// hindi: 'Number 4'
+// arabic: '5th most spoken language'
+// for all other simply log 'Great language too :D'
+
+const language2 = 'hindi'
+switch (language2) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break
+    case 'english':
+        console.log('3rd place');
+        break
+    case 'hindi':
+        console.log('Number 4');
+        break
+    case 'arabic':
+        console.log('5th most spoken language');
+        break
+    default:
+        console.log('Great language too :D');
+        break;
+}
+
+
+// LECTURE: The Conditional (Ternary) Operator
+// 1. If your country's population is greater than 33 million, use the ternary operator
+// to log a string like this to the console: 'Portugal's population is above average'.
+// Otherwise, simply log 'Portugal's population is below average'. Notice how only
+// one word changes between these two sentences!
+// 2. After checking the result, change the population temporarily to 13 and then to
+// 130. See the different results, and set the population back to original
+
+console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`);
+
+
+// Coding Challenge #4
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+// 300. If the value is different, the tip is 20%.
+// Your tasks:
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+// this. It's not allowed to use an if/else statement � (If it's easier for you, you can 
+// start with an if/else statement, and then try to convert it to a ternary 
+// operator!)
+// 2. Print a string to the console containing the bill value, the tip, and the final value 
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+// 316.25”
+// Test data:
+// § Data 1: Test for bill values 275, 40 and 430
+
+const bill = 430
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
