c0cdc9b812f84604d4e7ea706d122bbb0465055a
allisondev16 <allisonmamangun@gmail.com>
2022-06-08 06:55:04 +0800

    update script.js

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -162,3 +162,42 @@ const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(popu
 
 console.log(percentages);
 
+
+// LECTURE: Basic Array Operations (Methods)
+// 1. Create an array containing all the neighbouring countries of a country of your 
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array 
+// into a variable called 'neighbours'
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of 
+// your selected country. So add it to the end of the 'neighbours' array
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from 
+// the end of the array
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the 
+// console: 'Probably not a central European country :D'
+// 5. Change the name of one of your neighbouring countries. To do that, find the 
+// index of the country in the 'neighbours' array, and then use that index to 
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+// 1.
+const neighbours = ["Singapore", "Taiwan", "Malaysia"];
+console.log(neighbours);
+
+// 2.
+neighbours.push("Utopia");
+console.log(neighbours);
+
+// 3.
+neighbours.pop();
+console.log(neighbours);
+
+// // 4.
+if (!neighbours.includes("Germany")) {
+    console.log('Probably not a central European country :D');
+}
+
+// // 5.
+const index = neighbours.indexOf("Singapore");
+
+neighbours[index] = "SG"
+
+console.log(neighbours);
