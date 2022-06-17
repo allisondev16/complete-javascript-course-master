c4a17480db5c85cf59608f5b765dad94a7d377f2
allisondev16 <allisonmamangun@gmail.com>
2022-06-17 23:11:19 +0800

    update script.js

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -256,3 +256,14 @@ const myCountry = {
     population: 109.6,
     neighbors: neighbours
 }
+
+// Dot vs. Bracket Notation
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
