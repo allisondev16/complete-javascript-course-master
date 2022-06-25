bfe8829608eeba912f3ffe931790a6364b8425ee
allisondev16 <allisonmamangun@gmail.com>
2022-06-25 22:51:08 +0800

    update script.js

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -396,3 +396,7 @@ myCountry.describe();
 myCountry.checkIsland()
 console.log(myCountry);
 
+// LECTURE: Iteration: The for Loop
+// 1. There are elections in your country! In a small town, there are only 50 voters.
+// Use a for loop to simulate the 50 people voting, by logging a string like this to
+// the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
