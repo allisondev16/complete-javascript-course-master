90a25a4a91eda379fcc3e479630f9db5a0025fc6
allisondev16 <allisonmamangun@gmail.com>
2022-07-04 22:34:19 +0800

    update script.js

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -457,3 +457,9 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
 // for (let i = 0; i < listOfNeighbours.length; i++)
 //     for (let y = 0; y < listOfNeighbours[i].length; y++)
 //         console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+
+// LECTURE: The while Loop
+// 1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+// but this time using a while loop (call the array 'percentages3')
+// 2. Reflect on what solution you like better for this task: the for loop or the while
+// loop?
