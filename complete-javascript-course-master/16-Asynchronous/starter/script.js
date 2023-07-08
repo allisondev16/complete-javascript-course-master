53461008a1607b32e5ec43b54a22685384dff778
allisondev16 <allisonmamangun@gmail.com>
2023-07-08 18:38:25 +0800

    update

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -17,10 +17,10 @@ PART 1
 The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
 3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
 4. Chain a .catch method to the end of the promise chain and log errors to the console
-*5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
 
 PART 2
-6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+*6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
 7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
 
 TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
@@ -30,14 +30,93 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
-const whereAmI = (lat, lng) => {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(res => {
-      console.log(res);
-      return res.json();
-    })
-    .then(res => console.log(`You are in ${res.city}, ${res.country}`))
-    .catch(err => console.log(err));
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
 
-const test = whereAmI(52.508, 13.381);
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const getJSON = (url, errorMessage = 'Something went wrong.') => {
+  return fetch(url).then(data => {
+    console.log(data);
+    if (!data.ok) {
+      throw new Error(`${errorMessage} ${data.status}`);
+    }
+    return data.json();
+  });
+};
+
+const whereAmI = (lat, lng) => {
+  getJSON(`https://geocode.xyz/${lat},${lng}?geoit=json`, 'Error in geocoding.')
+    .then(res => {
+      console.log(res);
+      if (res.city === 'Throttled! See geocode.xyz/pricing') {
+        throw new Error('Throttled! 🤷‍♂️ See geocode.xyz/pricing');
+      }
+      console.log(`You are in ${res.city}, ${res.country}`);
+      return getJSON(
+        `https://countries-api-836d.onrender.com/countries/name/${res.country}`,
+        `Not found 🥲`
+      );
+    })
+    .then(data => {
+      renderCountry(data[0]);
+      console.log(data);
+    })
+    .catch(err => console.error(err.message));
+};
+
+const test = whereAmI(19.037, 72.873);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+const createImage = imgPath => {
+  return new Promise(resolve => {
+    const image = document.createElement('img');
+    document.getElementById('img').src = imgPath;
+    image.onload = function () {
+      alert('The image has loaded!');
+    };
+  });
+};
