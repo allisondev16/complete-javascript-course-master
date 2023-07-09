31d6ec88954a6cdcdfc498b672d67a8af462efa6
allisondev16 <allisonmamangun@gmail.com>
2023-07-09 15:14:52 +0800

    update

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -102,7 +102,7 @@ If this part is too tricky for you, just watch the first part of the solution.
 PART 2
 2. Comsume the promise using .then and also add an error handler;
 3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
-4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+*4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
 5. After the second image has loaded, pause execution for 2 seconds again;
 6. After the 2 seconds have passed, hide the current image.
 
@@ -111,12 +111,32 @@ TEST DATA: Images in the img folder. Test the error handler by passing a wrong i
 GOOD LUCK 😀
 */
 
+const wait = seconds => {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
 const createImage = imgPath => {
   return new Promise(resolve => {
     const image = document.createElement('img');
-    document.getElementById('img').src = imgPath;
+    image.src = imgPath;
     image.onload = function () {
-      alert('The image has loaded!');
+      console.log('The image has loaded.');
+      imgContainer.append(image);
+      resolve(image);
     };
   });
 };
+
+let imgElement;
+createImage(`img/img-1.jpg`)
+  .then(res => {
+    console.log(res);
+    imgElement = res;
+    return wait(2);
+  })
+  .then(() => console.log(imgElement))
+  .catch(err => console.error(err));
